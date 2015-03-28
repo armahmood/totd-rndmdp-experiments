71013a081fc25279c2f2f2c0e74053f28bc28d51
Ashique Rupam Mahmood <ashique@ualberta.ca>
2015-03-27 20:18:34 -0400

    Plot data processing code is added with test

---
+++ pysrc/plot/plotdataprocess.py
@@ -0,0 +1,132 @@
+'''
+Created on Jan 25, 2015
+
+@author: A. Rupam Mahmood
+
+Responsibility of this program is to take several output files
+from different runs for a particular algorithm on an experiment
+and extract simple tables containing performance vs. a given
+set of parameter by averaging over the runs and choosing the
+best values for the rest of the parameters. This table can then
+be easily loaded up and plotted using standard matplotlib tools.
+
+'''
+
+import numpy as np
+import cPickle as pickle
+import itertools
+import sys
+
+
+def loaddata(nruns, path):
+  data = []
+  for run in range(1, nruns + 1):
+    filepathname = path + "run_" + str(run) + ".dat"
+    f = open(filepathname, 'rb')
+    try:
+      while True:
+        d = pickle.load(f)
+        data.append(d)
+    
+    except EOFError:
+      print 'End of file reached'
+  return data
+
+def createtable(data, params, neps):
+  table       = np.zeros((len(data), len(params)+neps))
+  nparams     = len(params)
+
+  for i in range(len(data)):
+    for j in range(nparams):
+      table[i,j]    = data[i][params[j]]
+    table[i,nparams:]   = data[i]['mse']
+  return table
+
+def createtableavg(table, nruns, neps):
+  (tablerows, tablecols)      = np.shape(table)
+  tableavgrows                = tablerows/nruns
+  nparams                     = tablecols-neps
+  tableavgcols                = nparams+2
+  tableavgstd                 = np.zeros((tableavgrows, tableavgcols))
+  tabletemp                   = np.zeros((tableavgrows, neps))
+  tableavgstd[:, :nparams]  = table[:tableavgrows, :nparams]
+  tabletemp[:,:neps]          = table[:tableavgrows, nparams:]
+  
+  for i in range(1, nruns):
+    tabletemp = np.concatenate((tabletemp, \
+                table[(i)*tableavgrows:(i+1)*tableavgrows, nparams:]), 1)
+    #print np.shape(tabletemp)
+    #print np.shape(table[(i)*tableavgrows:(i+1)*tableavgrows, nparams:])
+    
+  tableavgstd[:, nparams] = np.mean(tabletemp, 1)
+  tableavgstd[:, nparams+1] = np.std(tabletemp, 1)/np.sqrt(neps*nruns)
+  
+  return tableavgstd
+  
+def performancevsparams(tableavgstd, params, paramssub):
+  
+  (tableavgrows, tableavgcols)    = np.shape(tableavgstd)
+
+  paramvals = {}
+  
+  nparamssubvals = 1.
+  for param in params:
+      paramvals[param]   = np.unique(tableavgstd[:, param==params])
+      if (param==paramssub).any():
+        nparamssubvals *= len(paramvals[param])
+  
+  paramsubvalcomblist = list(itertools.product(\
+                      *[paramvals[param] for param in paramssub ]))
+
+  perftable           = np.zeros((len(paramsubvalcomblist), \
+                tableavgcols - len(params)+len(paramssub)))
+  row = 0
+  for paramsubvalcomb in paramsubvalcomblist:
+    paramsubvalcomb = np.array(paramsubvalcomb)
+    condition = np.array(np.repeat(True, tableavgrows))
+    for param in params:
+      if (param==paramssub).any():
+        condition = condition * \
+          (tableavgstd[:,param==params] == paramsubvalcomb[param==paramssub])\
+            .reshape(tableavgrows)
+    perftable[row,:len(paramssub)] = paramsubvalcomb
+    perftable[row,len(paramssub)] = np.nanmin(tableavgstd[condition,len(params)])
+    perftable[row,len(paramssub)+1] = np.nanmin(tableavgstd[condition,len(params)+1])
+    
+    row += 1
+    
+  return perftable
+  
+def main():
+  nruns = 10
+  path        = "/Users/ashique/results/projects/wisOnexps/results/exprwbinary/wgtd/"
+  params      = np.array(['eta', 'initd', 'beta', 'lambda'])
+  paramssub   = np.array(['lambda'])
+
+  if len(sys.argv)>1:
+    nruns  = int(sys.argv[1])
+    path   = sys.argv[2]
+    nparams = int(sys.argv[3])
+    params = np.array([ sys.argv[4+i] for i in range(nparams) ])
+    nparamssub = int(sys.argv[4+nparams]) 
+    paramssub = np.array( [ sys.argv[4+nparams+1+i] for i in range(nparamssub) ] )
+  
+  data        = loaddata(nruns, path)
+  neps        = data[0]['neps']
+  table       = createtable(data, params, neps)
+  tableavgstd = createtableavg(table, nruns, neps)
+  
+  perftable = performancevsparams(tableavgstd, params, paramssub)
+  
+  print perftable    
+  fsname    = path+'perfvs'
+  for i in range(len(paramssub)): fsname += paramssub[i]
+  fsname    += ".plot"
+  fs           = open(fsname, "wb")
+  pickle.dump(perftable, fs)
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
---
+++ pysrctest/plot/plotdataprocesstest.py
@@ -0,0 +1,185 @@
+'''
+Created on Mar 27, 2015
+
+@author: A. Rupam Mahmood
+'''
+import unittest
+import numpy as np
+import pysrc.plot.plotdataprocess as plotutil
+from pyparsing import alphas
+
+class Test(unittest.TestCase):
+
+  def createdummydata(self,alphas,betas,lmbdas,mse,runs):
+    '''
+    This method creates a dummy data object, which is a list 
+    of configurations. In plotutil, it is obtained from different
+    output files (based on different runs) and concatenated together. 
+    This method can be used to avoid the need to have some already 
+    created output files. An example of the arguments it requires
+    is as follows:
+    
+    alphas  = np.array([0, 0.5, 1.0])
+    betas   = np.array([0, 0.1])
+    lmbdas = np.array([0, 0.99])
+    # Mse here is somewhat wrong. another dimension is required 
+    mse     = np.array([
+                        [[0.11,0.09], [0.41,0.39], [0.71,0.69], [1.01,1.09]],
+                        [[0.21,0.19], [0.51,0.49], [0.81,0.79], [1.11,1.09]],
+                        [[0.31,0.29], [0.61,0.59], [0.91,0.89], [1.21,1.19]]
+                        ]
+                       )
+    runs = np.array([1,2,3])
+    '''
+    data = [
+               {'mse'   : mse[alpha==alphas,beta==betas,lmbda==lmbdas]+run,
+                'alpha' : alpha,
+                'beta'   : beta,
+                'lmbda'  : lmbda,
+                'x1'     : 0.1}
+               for run in runs
+               for alpha in alphas
+               for beta in betas
+               for lmbda in lmbdas
+               ]
+    return data
+
+  def tableexample1(self):
+    '''
+    This method creates a small table with 2 episodes 
+    in a form created by createtable of plotutil.
+     
+    '''
+    
+    alphas  = np.array([0, 0.5, 1.0])
+    betas   = np.array([0, 1.0])
+    lmbdas  = np.array([0])
+    mse     = np.array([
+                        [[[0.11,0.09]], [[0.41,0.39]] ],
+                        [[[0.21,0.19]], [[0.51,0.49]] ],
+                        [[[0.31,0.29]], [[0.61,0.59]] ]
+                        ]
+                       )
+    runs    = np.array([1,2,3])
+    table   = np.array([ [alpha, beta, lmbda, 
+                          mse[alpha==alphas,beta==betas,lmbda==lmbdas,0]+run,
+                          mse[alpha==alphas,beta==betas,lmbda==lmbdas,1]+run  
+                          ] 
+                       for run in runs
+                       for alpha in alphas
+                       for beta in betas
+                       for lmbda in lmbdas
+                        ])
+    nrows, ncols    = np.shape(table) 
+    nruns           = len(runs)
+    tableavgstd        = np.zeros((nrows/nruns, 5))
+    tableavgstd[:,:3]  = table[:nrows/nruns,:3]
+    i = 0
+    for alpha in alphas:
+      for beta in betas:
+        for lmbda in lmbdas:
+          temp = np.array([1+mse[alpha==alphas,beta==betas,lmbda==lmbdas],\
+                      2+mse[alpha==alphas,beta==betas,lmbda==lmbdas],\
+                      3+mse[alpha==alphas,beta==betas,lmbda==lmbdas],\
+                      ])
+          tableavgstd[i,3] = np.mean(temp)
+          tableavgstd[i,4] = np.std(temp)/np.sqrt(2*nruns)
+          i += 1
+    return {'alphas':alphas, 'betas':betas, 
+            'lmbdas':lmbdas, 'mse':mse, 
+            'runs':runs, 'table':table, 
+            'tableavgstd':tableavgstd}
+
+
+  def tableexample1explicit(self):
+    tableavgstd2 = np.array([[0., 0., 0., 2.1, 0.33335833], 
+        [0., 1.0, 0., 2.4, 0.33335833], 
+        [0.5, 0., 0., 2.2, 0.33335833], 
+        [0.5, 1.0, 0., 2.5, 0.33335833], 
+        [1.0, 0., 0., 2.3, 0.33335833], 
+        [1.0, 1.0, 0., 2.6, 0.33335833]])
+    perfvsalpha       = np.array(
+                           [[0,    2.1],
+                            [0.5,  2.2],
+                            [1.0,  2.3]]
+                           )
+    perfvsbeta        = np.array(
+                           [[0,    2.1],
+                            [1.0,  2.4]]
+                           )
+    perfvslmbda       = np.array([[0,   2.1]])
+    perfvsalphabeta   = np.array([
+                                 [0.,   0.,  2.1],
+                                 [0.,   1.0, 2.4],
+                                 [0.5,  0.,  2.2],
+                                 [0.5,  1.0, 2.5],
+                                 [1.0,  0.,  2.3],
+                                 [1.0,  1.0, 2.6]]                                 
+                                 )
+    perfvsbetaalpha   = np.array([
+                                 [0.,   0.,   2.1],
+                                 [0.,   0.5,  2.2],
+                                 [0.,   1.0,  2.3],
+                                 [1.0,  0.0,  2.4],
+                                 [1.0,  0.5,  2.5],
+                                 [1.0,  1.0,  2.6]]                                 
+                                 )
+    return {'tableavgstd2':tableavgstd2, 'perfvsalpha':perfvsalpha,
+            'perfvsbeta':perfvsbeta, 'perfvslmbda':perfvslmbda,
+            'perfvsalphabeta':perfvsalphabeta, 'perfvsbetaalpha':perfvsbetaalpha}
+
+  def testtableexample1(self):
+    rets          = self.tableexample1()
+    tableavgstd   = rets['tableavgstd']
+    rets          = self.tableexample1explicit()
+    tableavgstd2  = rets['tableavgstd2']
+    assert((abs(tableavgstd-tableavgstd2)<0.000001).all())
+  
+  def testcreatetable(self):
+    rets      = self.tableexample1()
+    alphas=rets['alphas']; betas=rets['betas'];lmbdas=rets['lmbdas']
+    mse=rets['mse']; runs=rets['runs']; table=rets['table']
+    data      = self.createdummydata(alphas,betas,lmbdas,mse,runs)
+    params    = np.array(['alpha', 'beta', 'lmbda'])
+    table2    = plotutil.createtable(data, params, 2)
+    assert(((table-table2)==0.0).all())
+
+  def testcreatetableavg(self):
+    rets      = self.tableexample1()
+    alphas=rets['alphas']; betas=rets['betas'];lmbdas=rets['lmbdas']
+    mse=rets['mse']; runs=rets['runs']; tableavgstd=rets['tableavgstd']
+    params        = np.array(['alpha', 'beta', 'lmbda'])
+    data          = self.createdummydata(alphas,betas,lmbdas,mse,runs)
+    table         = plotutil.createtable(data, params, 2)
+    tableavgstd2  = plotutil.createtableavg(table, len(runs), 2)
+    assert(((tableavgstd-tableavgstd2)==0.0).all())
+    
+  def testperformancevsparams(self):
+    rets      = self.tableexample1()
+    alphas=rets['alphas']; betas=rets['betas'];lmbdas=rets['lmbdas']
+    mse=rets['mse']; runs=rets['runs']; tableavgstd=rets['tableavgstd']
+    data          = self.createdummydata(alphas,betas,lmbdas,mse,runs)
+    params        = np.array(['alpha', 'beta', 'lmbda'])
+    table         = plotutil.createtable(data, params, 2)
+    tableavgstd   = plotutil.createtableavg(table, len(runs), 2)
+    perfvsalpha2  = plotutil.performancevsparams(tableavgstd, params, np.array(['alpha']))
+    perfvsbeta2   = plotutil.performancevsparams(tableavgstd, params, np.array(['beta']))
+    perfvslmbda2  = plotutil.performancevsparams(tableavgstd, params, np.array(['lmbda']))
+    perfvsalphabeta2  = plotutil.performancevsparams(tableavgstd, params, np.array(['alpha', 'beta']))
+    perfvsbetaalpha2  = plotutil.performancevsparams(tableavgstd, params, np.array(['beta', 'alpha']))
+    rets            = self.tableexample1explicit()
+    perfvsalpha     = rets['perfvsalpha']
+    perfvsbeta      = rets['perfvsbeta']
+    perfvslmbda     = rets['perfvslmbda']
+    perfvsalphabeta = rets['perfvsalphabeta']
+    perfvsbetaalpha = rets['perfvsbetaalpha']
+    assert((abs(perfvsalpha[:,:2] - perfvsalpha2[:,:2])<0.000001).all())
+    assert((abs(perfvsbeta[:,:2] - perfvsbeta2[:,:2])<0.000001).all())
+    assert((abs(perfvslmbda[:,:2] - perfvslmbda2[:,:2])<0.000001).all())
+    assert((abs(perfvsalphabeta[:,:2] - perfvsalphabeta2[:,:2])<0.000001).all())
+    assert((abs(perfvsbetaalpha[:,:2] - perfvsbetaalpha2[:,:2])<0.000001).all())
+    
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testcreatetableavg']
+    unittest.main()
+    
